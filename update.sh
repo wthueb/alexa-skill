0964ab6c427d2069cee391578b1928b039ee318d
William Huebner <wilhueb@gmail.com>
2019-10-20 03:26:04 -0400

    add secrets to zip file to upload

--- update.sh
+++ update.sh
@@ -5,4 +5,5 @@ cd env/lib/python3.7/site-packages/
 zip -r ../../../../function.zip .
 cd ../../../../
 zip -g function.zip index.py
+zip -g function.zip secrets.py
 aws lambda update-function-code --function-name alexa-http-request --zip-file fileb://function.zip
