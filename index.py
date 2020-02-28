b50773272a87a06ea926d99d162c822c331a237d
William Huebner <wilhueb@gmail.com>
2020-02-28 03:03:49 -0500

    remove unused imports

--- index.py
+++ index.py
@@ -4,13 +4,10 @@ import logging
 import requests
 
 from ask_sdk_core.skill_builder import SkillBuilder
-from ask_sdk_core.dispatch_components import (
-        AbstractRequestHandler, AbstractExceptionHandler,
-        AbstractRequestInterceptor, AbstractResponseInterceptor)
+from ask_sdk_core.dispatch_components import (AbstractRequestHandler, AbstractExceptionHandler)
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
 
-from ask_sdk_model.ui import SimpleCard
 from ask_sdk_model import Response
 
 import config
